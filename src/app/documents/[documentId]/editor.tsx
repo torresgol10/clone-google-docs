04b19d32c6baa24bd8997aa72949d3484d80fb56
torresgol10.itd <torresgol10.itd@gmail.com>
2024-12-06 21:52:04 +0000

    Creamos un store para el editor

--- src/app/documents/[documentId]/editor.tsx
+++ src/app/documents/[documentId]/editor.tsx
@@ -10,10 +10,16 @@ import TableHeader from '@tiptap/extension-table-header'
 import TableRow from '@tiptap/extension-table-row'
 import Image from '@tiptap/extension-image'
 import ImageResize from 'tiptap-extension-resize-image';
+import { useEditorStore } from '@/store/use-editor-store'
 
 
 export default function Editor() {
+  const { setEditor } = useEditorStore()
+
   const editor = useEditor({
+    onCreate({ editor }) {
+      setEditor(editor)
+    },
     editorProps: {
       attributes: {
         style: "padding-left: 56px; padding-right: 56px;",
